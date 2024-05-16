400c15c90c10bac6e89efbe91ec758de516cdc0d
Igor2kLevi <99228640+Igor2kLevi@users.noreply.github.com>
2024-05-16 20:03:37 -0300

    correct bug2

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
         element: <Home />
       },
       {
-        path: "/battle-simulator",
+        path: "/battleSimulator",
         element: <Battle />
       }
     ]
